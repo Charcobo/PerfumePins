d7a5d543f5920cad3531b739c9fafb4cda97ef52
Charlie Logan <charles.logan@disney.com>
2019-05-26 12:02:25 -0700

    Model check-in

---
+++ src/main/java/com/perfumepins/model/Pin.java
@@ -0,0 +1,43 @@
+package com.perfumepins.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Pin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pinId;
+    private String pinName;
+    private String pinNumber;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "pins")
+    private Set<User> users;
+
+    public Integer getPinId() {
+        return pinId;
+    }
+
+    public String getPinName() {
+        return pinName;
+    }
+
+    public String getPinNumber() {
+        return pinNumber;
+    }
+
+    public Set<User> getUsers() { return users; }
+
+    public void setPinName(String pinName) {
+        this.pinName = pinName;
+    }
+
+    public void setPinNumber(String pinNumber) {
+        this.pinNumber = pinNumber;
+    }
+
+    public void setUsers(Set<User> users) { this.users = users; }
+}
---
+++ src/main/java/com/perfumepins/model/User.java
@@ -0,0 +1,42 @@
+package com.perfumepins.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    private String username;
+
+    @JsonIgnore
+    @ManyToMany(cascade = { CascadeType.PERSIST })
+    @JoinTable(
+            name = "user_x_pin",
+            joinColumns = { @JoinColumn(name = "user_id") },
+            inverseJoinColumns = { @JoinColumn(name = "pin_id") }
+    )
+    private List<Pin> pins;
+
+    public void addPin(Pin pin) { pins.add(pin); }
+
+    public void addPins(ArrayList<Pin> pins) { pins.addAll(pins); }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<Pin> getPins() { return pins; }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
