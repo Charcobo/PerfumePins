33673dff7e8b186bb75a9cddbf140242372d378c
Charlie Logan <charles.logan@disney.com>
2019-05-26 12:02:10 -0700

    Controller check-in

---
+++ src/main/java/com/perfumepins/controller/PinController.java
@@ -0,0 +1,57 @@
+package com.perfumepins.controller;
+
+import com.perfumepins.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import com.perfumepins.model.Pin;
+import com.perfumepins.repository.PinRepository;
+
+import java.util.*;
+
+@Controller
+@RequestMapping(path = "/perfume/pin")
+public class PinController {
+    @Autowired
+    private PinRepository pinRepository;
+
+    // http://<hostname>/perfume/pin/
+    // Returns a list of all pins
+    @GetMapping("/")
+    public @ResponseBody Iterable<Pin> getAllPins() {
+        return pinRepository.findAll();
+    }
+
+    // http://<hostname>/perfume/pin?pinName=<partial pin name>
+    // Returns a list of pins containing the specified pinName
+    @GetMapping("")
+    public @ResponseBody ArrayList<Pin> getPinsByPinName(@RequestParam (value = "pinName") String pinName) {
+        return pinRepository.findPinsByPinNameContains(pinName);
+    }
+
+    // http://<hostname>/perfume/pin/<pinId>
+    // Returns a single pin specified by the pinId
+    @GetMapping("/{pinId}")
+    public @ResponseBody Optional<Pin> getPinById(@PathVariable Integer pinId) {
+        return pinRepository.findById(pinId);
+    }
+
+    // http://<hostname>/perfume/pin/<pinId>/users
+    // Returns a list of users who own the pin specified by pinId
+    @GetMapping("/{pinId}/users")
+    public @ResponseBody Set<User> getPinsUsers(@PathVariable Integer pinId) {
+        Optional<Pin> pin = pinRepository.findById(pinId);
+        Set<User> pinUsers = pin.get().getUsers();
+        return pinUsers;
+    }
+
+    // POST - http://<hostname>/perfume/pin/add
+    // Adds a pin
+
+    // PUT/PATCH - http://<hostname>/perfume/pin/<pinId>
+    // Updates a pin specified by pinId
+
+    // DELETE - http://<hostname>/perfume/pin/<pinId>
+    // Deletes a pin specified by pinId (Will need to clean up the cross table entries as well)
+}
---
+++ src/main/java/com/perfumepins/controller/UserController.java
@@ -0,0 +1,101 @@
+package com.perfumepins.controller;
+
+import com.perfumepins.model.Pin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import com.perfumepins.model.User;
+import com.perfumepins.repository.UserRepository;
+
+import java.util.*;
+
+@Controller
+@RequestMapping(path = "/perfume/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+//    @Autowired
+//    private UserXPinRepository userXPinRepository;
+
+    // GET - http://<hostname>/perfume/user/
+    // Returns a list of all users
+    @GetMapping("/")
+    public @ResponseBody
+    Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // GET - http://<hostname>/perfume/user?username=<username>
+    // Returns a list of users containing the specified username
+    @GetMapping("")
+    public @ResponseBody ArrayList<User> getUserByUsername(@RequestParam (value = "username") String username) {
+        return userRepository.findUsersByUsernameContains(username);
+    }
+
+    // GET - http://<hostname>/perfume/user/<userId>
+    // Returns a single user specified by the userId
+    @GetMapping("/{userId}")
+    public @ResponseBody Optional<User> getUserById(@PathVariable Integer userId) {
+        return userRepository.findById(userId);
+    }
+
+    // GET - http://<hostname>/perfume/user/<userId>/pins
+    // Returns a sorted list of pins owned by the user specified by userId
+    @GetMapping("/{userId}/pins")
+    public @ResponseBody List<Pin> getUserPins(@PathVariable Integer userId) {
+        Optional<User> user = userRepository.findById(userId);
+        List<Pin> userPins = user.get().getPins();
+        Comparator<Pin> compareByPinId = (p1, p2) -> p1.getPinId().compareTo(p2.getPinId());
+        Collections.sort(userPins, compareByPinId);
+        return userPins;
+    }
+
+    // POST - http://<hostname>/perfume/user/add
+    // Adds a user
+    @PostMapping(value = "/add") //, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String addUser(
+            @RequestBody User userData) {
+        User newUser = new User();
+        newUser.setUsername(userData.getUsername());
+        userRepository.save(newUser);
+        User user = userRepository.findById(newUser.getUserId()).get();
+        return "New user " + user.getUsername() + " has been added.";
+    }
+
+    // POST - http://<hostname>/perfume/user/<userId>/pins/add
+    // Adds a pin specified by request body to a user specified by userId
+    @PostMapping(value = "/{userId}/pins/add") //, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String addPinToUserPins(
+            @PathVariable Integer userId,
+            @RequestBody Pin pin) {
+        User user = userRepository.findById(userId).get();
+        user.addPin(pin);
+        userRepository.save(user);
+        return "Pin " + pin.getPinId() + " added to " + user.getUsername() + "'s pin collection.";
+    }
+
+    // POST - http://<hostname>/perfume/user/<userId>/pins/add
+    // Adds a pin specified by request body to a user specified by userId
+    @PostMapping(value = "/{userId}/pins/add-all") //, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String addPinsToUserPins(
+            @PathVariable Integer userId,
+            @RequestBody ArrayList<Pin> pins) {
+        User user = userRepository.findById(userId).get();
+        user.addPins(pins);
+        userRepository.save(user);
+        return "Pins added to " + user.getUsername() + "'s pin collection.";
+    }
+
+    // PUT/PATCH - http://<hostname>/perfume/user/<userId>
+    // Updates a user specified by userId
+
+    // DELETE - http://<hostname>/perfume/user/<userId>
+    // Deletes a user specified by userId (Will need to clean up the cross table entries as well)
+
+    // DELETE - http://<hostname>/perfume/user/<userId>/pin/<pinId>
+    // Deletes a pin specified by pinId from a user specified by userId (But from which row if multiple?)
+}
