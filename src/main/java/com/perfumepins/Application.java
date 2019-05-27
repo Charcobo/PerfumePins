7fb56d94f2b52cc941a5d539bb465854ebe73b8d
Charlie Logan <charles.logan@disney.com>
2019-05-26 20:55:35 -0700

    Deleted Hibernate version log message

--- src/main/java/com/perfumepins/Application.java
+++ src/main/java/com/perfumepins/Application.java
@@ -10,7 +10,6 @@ public class Application {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hibernate version is: " + org.hibernate.Version.getVersionString());
         SpringApplication.run(Application.class, args);
     }
 }
